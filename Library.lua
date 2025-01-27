9bca4008065d5b085c02f6e2b886c26d7c79df6d
deividcomsono <116926351+deividcomsono@users.noreply.github.com>
2025-01-27 12:18:30 -0300

    mango

--- Library.lua
+++ Library.lua
@@ -1,6 +1,10 @@
 local cloneref = (cloneref or clonereference or function(instance: any)
 	return instance
 end)
+local getgenv = getgenv or function()
+	return {}
+end
+
 local CoreGui: CoreGui = cloneref(game:GetService("CoreGui"))
 local Players: Players = cloneref(game:GetService("Players"))
 local RunService: RunService = cloneref(game:GetService("RunService"))
