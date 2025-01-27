31eca5ec7c6611bfed80b914a7102be42a8416d4
deividcomsono <116926351+deividcomsono@users.noreply.github.com>
2025-01-27 12:20:48 -0300

    silence clipboard

--- Library.lua
+++ Library.lua
@@ -4,6 +4,7 @@ end)
 local getgenv = getgenv or function()
 	return {}
 end
+local setclipboard = setclipboard or nil
 
 local CoreGui: CoreGui = cloneref(game:GetService("CoreGui"))
 local Players: Players = cloneref(game:GetService("Players"))
